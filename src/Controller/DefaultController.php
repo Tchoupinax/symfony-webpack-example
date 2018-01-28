3c87dea21cfb7e6a1466198b775f553e49668aae
Tchoupinax <corentinfiloche@hotmail.fr>
2018-01-28 11:43:52 +0100

    minimal configuration to display a page with symfony

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+
+class DefaultController extends Controller
+{
+    public function index()
+    {
+        return $this->render('base.html.twig');
+    }
+}
